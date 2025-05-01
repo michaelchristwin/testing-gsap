843523630d7a1fee7c6e7891ec2c912d5d32376b
michaelchristwin <kelechukwuchristwin@gmail.com>
2025-05-01 07:36:46 +0100

    Fixed island zoom animation

--- app/routes/gsap.tsx
+++ app/routes/gsap.tsx
@@ -75,7 +75,7 @@ const Page = () => {
       tl.fromTo(
         ".island",
         { opacity: 0, scale: 0 },
-        { opacity: 1, scale: 3, ease: "power1.out" },
+        { opacity: 1, scale: 5, duration: 2.5, ease: "power1.out" },
         1
       );
 
